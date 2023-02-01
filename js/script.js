de94f695ca28f277413ed35638cd3097efec28d1
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-01 16:06:57 +0100

    FEAT codice js per recuperare valore degli INPUT ... ... dopo aver interagito con pulsante

--- js/script.js
+++ js/script.js
@@ -3,4 +3,19 @@ const kmElement = document.getElementById("km");
 const etaElement = document.getElementById("eta");
 const calcolaElement = document.getElementById("calcola");
 
-console.log(km,eta,calcola);
+console.log(kmElement,etaElement,calcolaElement);
+
+// 2. Recuperare valori degli elementi INPUT quando si interagisce con il pulsante "Calcola"
+
+calcolaElement.addEventListener("click", function() {
+
+  const km = parseFloat(kmElement.value);
+  const eta = etaElement.value;
+  console.log("KM: ",km);
+  console.log("Età: ",eta);
+
+  
+
+});
+
+
