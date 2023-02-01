ef255260e872ab212321a6de18d88e96d230b6e7
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-01 16:23:48 +0100

    FEAT codice js per calcolo Prezzo Biglietto

--- js/script.js
+++ js/script.js
@@ -14,7 +14,31 @@ calcolaElement.addEventListener("click", function() {
   console.log("KM: ",km);
   console.log("Età: ",eta);
 
-  
+  // 3. Calcolare PREZZO BASE (km * 0.21€) del Biglietto
+  const PREZZO_PER_KM = 0.21;
+  let prezzoBase = km * PREZZO_PER_KM;
+
+  // 4. Calcolare PREZZO SCONTATO in base a Età
+  // 5. Stampare PREZZO su "console.log"
+  let prezzoScontato;
+
+  switch(eta){
+
+    case "minorenne":
+      prezzoScontato = prezzoBase * 0.8
+      console.log("Il prezzo scontato per i minorenni è: ",prezzoScontato);
+    break;
+
+    case "senior":
+      prezzoScontato = prezzoBase * 0.6;
+      console.log("Il prezzo scontato per i senior è: ",prezzoScontato);
+    break;
+
+    case "intero":
+      console.log("Il prezzo intero del tuo biglietto è: ",prezzoBase);
+    break;
+
+  }
 
 });
 
