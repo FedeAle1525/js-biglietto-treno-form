05bd7fddeef6a2825b137d83bcd6d825b719ffc5
FedeAle1525 <fedeale.1525@gmail.com>
2023-02-01 15:49:10 +0100

    FEAT codice js per recuperare elementi INPUT .... ... da pagina HTML

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. Recuperare elementi INPUT da pagine HTML
+const kmElement = document.getElementById("km");
+const etaElement = document.getElementById("eta");
+const calcolaElement = document.getElementById("calcola");
+
+console.log(km,eta,calcola);
